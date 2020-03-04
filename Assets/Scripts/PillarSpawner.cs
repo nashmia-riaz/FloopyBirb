cb43b682d5a87bed1c8c7c7c788b78a0d9352481
nashmia-riaz <nashmia-riaz@hotmail.com>
2020-03-04 15:19:36 +0000

    Second tutorial: First line of code!

---
+++ Assets/Scripts/PillarSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarSpawner : MonoBehaviour
+{
+    public GameObject pillarPrefab;
+
+    public Transform spawnPoint;
+
+    float timer, spawnTime;
+
+    public float YMin, YMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        spawnTime = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //incrementing our timer
+        timer += Time.deltaTime;
+
+        //execute code once timer runs out
+        if(timer > spawnTime)
+        {
+            //resets our timer
+            timer = 0;
+
+            float randomYPosition = Random.Range(YMin, YMax);
+
+            Vector3 pillarPosition = spawnPoint.position + new Vector3(0, randomYPosition, 0);
+
+            //creating pillars
+            Instantiate(pillarPrefab, pillarPosition, pillarPrefab.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+
+    public float upForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the left mouse button is clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigidbody.velocity = new Vector2(0, 0);
+            rigidbody.AddForce(new Vector2(0, upForce));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Player collided with " + collision.gameObject.name);
+    }
+}
